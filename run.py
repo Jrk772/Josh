80cdcd54be7fa5910bea6dfb505b12d5de28aa0a
Jrk772 <Jrk30@proton.me>
2025-11-27 20:03:26 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,8 @@
 from fastapi import FastAPI
-from mediaflow_proxy.main import app as mediaflow_app  # Import mediaflow app
-import httpx
-import re
-import string
+from mediaflow_proxy.main import app as mediaflow_app
 
-# Initialize the main FastAPI application
+# App principale esposta a Hugging Face
 main_app = FastAPI()
 
-# Manually add only non-static routes from mediaflow_app
-for route in mediaflow_app.routes:
-    if route.path != "/":  # Exclude the static file path
-        main_app.router.routes.append(route)
-
-# Run the main app
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(main_app, host="0.0.0.0", port=8080)
+# Monta direttamente MediaFlow Proxy sotto "/"
+main_app.mount("/", mediaflow_app)
